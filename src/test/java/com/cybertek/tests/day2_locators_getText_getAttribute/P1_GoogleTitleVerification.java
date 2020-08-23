f94e3ff32275356a3834609b69316f24da9d4d71
gurhank <gurhankocyigit@gmail.com>
2020-08-23 10:31:10 -0500

    day2 practice1

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P1_GoogleTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P1_GoogleTitleVerification.java
@@ -1,12 +1,25 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P1_GoogleTitleVerification {
 
     public static void main(String[] args) {
 
         //TC #1: Google Title Verification
-        //1. Open Chrome browser
-        //2. Go to https://www.google.com 3. Verify title:
+        // 1. Open Chrome browser
+        //set up the browser driver
+        WebDriverManager.chromedriver().setup();
+        //if you are using browser drivers locally, you need to use System.setProperty everytime
+        //we use WebDriverManager
+
+        //open a chrome browser
+        WebDriver driver = new ChromeDriver();
+        
+        // 2. Go to https://www.google.com
+        // 3. Verify title:
         //Expected: Google
 
 
