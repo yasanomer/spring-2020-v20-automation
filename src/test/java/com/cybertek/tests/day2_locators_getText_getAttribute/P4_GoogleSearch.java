4793615594e2e2e39b88887227e7db6ca7e2a8ad
gurhank <gurhankocyigit@gmail.com>
2020-08-23 14:13:09 -0500

    day2

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P4_GoogleSearch.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P4_GoogleSearch.java
@@ -2,6 +2,7 @@ package com.cybertek.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -24,11 +25,21 @@ public class P4_GoogleSearch {
         // To be able to write anything in the search box
         // I need to locate the search box and use sendKeys() method to send "apple"
 
-        driver.findElement(By.name("q")).sendKeys("apple");
+        //<----finding the search box<--||-> sending the keys <-----------
+        driver.findElement(By.name("q")).sendKeys("apple" + Keys.ENTER);
 
         //4- Click google search button
         //5- Verify title:
-        //Expected: Title should start with “apple” word
+        //Expected: Title should be "apple - Google Search"
+            String expectedTitle = "apple - Google Search";
+            String actualTitle = driver.getTitle();
+
+            if (actualTitle.equals(expectedTitle)){
+                System.out.println("Search title verification PASSED!");
+            }else{
+                System.out.println("Search title verification FAILED!!!");
+            }
+
 
 
 
