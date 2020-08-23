500f3b042ce7ae2e21c7362949d798e9a3b43619
gurhank <gurhankocyigit@gmail.com>
2020-08-23 10:27:51 -0500

    day2 practice1

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P1_GoogleTitleVerification.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class P1_GoogleTitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #1: Google Title Verification
+        //1. Open Chrome browser
+        //2. Go to https://www.google.com 3. Verify title:
+        //Expected: Google
+
+
+    }
+
+}
