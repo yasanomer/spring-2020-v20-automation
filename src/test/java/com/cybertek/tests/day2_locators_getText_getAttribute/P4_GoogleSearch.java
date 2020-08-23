20c8e7816fc7e8ad2af9068afbe2098ff8d22ad4
gurhank <gurhankocyigit@gmail.com>
2020-08-23 13:50:34 -0500

    day2

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P4_GoogleSearch.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P4_GoogleSearch.java
@@ -1,12 +1,24 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P4_GoogleSearch {
 
     public static void main(String[] args) {
 
        //TC #4: Google search
         //1- Open a chrome browser
+        //setup the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //create driver instance and open chrome browser
+        WebDriver driver = new ChromeDriver();
+
         //2- Go to: https://google.com
+        driver.get("https://google.com");
+
         //3- Write “apple” in search box
         //4- Click google search button
         //5- Verify title:
