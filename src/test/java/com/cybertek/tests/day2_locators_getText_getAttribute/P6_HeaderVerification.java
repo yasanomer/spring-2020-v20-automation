89d718bc989b4780b305e964be832016ad12f8f3
gurhank <gurhankocyigit@gmail.com>
2020-08-23 15:40:40 -0500

    day 2 practice 6

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P6_HeaderVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P6_HeaderVerification.java
@@ -1,4 +1,26 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P6_HeaderVerification {
+
+    public static void main(String[] args) {
+
+        //TC #6: Zero Bank header verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+
+        //open browser
+        WebDriver driver = new ChromeDriver();
+
+        //2. Go to http://zero.webappsecurity.com/login.html
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank”
+
+    }
+
 }
