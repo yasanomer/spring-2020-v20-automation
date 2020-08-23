a809851ee8ef5da421d095cd47fc290dffc5792e
gurhank <gurhankocyigit@gmail.com>
2020-08-23 10:55:23 -0500

    day2 practice2

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P2_CybertekURLVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P2_CybertekURLVerification.java
@@ -1,11 +1,24 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P2_CybertekURLVerification {
 
     public static void main(String[] args) {
 
         //TC #2: Cybertek verifications
         //1. Open Chrome browser
+        //I need to setup my browser driver.
+        WebDriverManager.chromedriver().setup();
+
+        //To create instance of chrome browser from WebDriver.
+        WebDriver driver = new ChromeDriver();
+
+        // this line will maximize the newly opened browser page
+        driver.manage().window().maximize();
+
         //2. Go to https://practice.cybertekschool.com
         //3. Verify URL contains
         //Expected: cybertekschool
