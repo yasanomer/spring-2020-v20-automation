8fead9928674f0a8906b3f49e5696453f86d9052
gurhank <gurhankocyigit@gmail.com>
2020-09-01 13:42:54 -0500

    facebook task solution

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/F3_HeaderVerification.java
@@ -0,0 +1,4 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class F3_HeaderVerification {
+}
---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/F4_AttributeValueVerification.java
@@ -0,0 +1,4 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class F4_AttributeValueVerification {
+}
