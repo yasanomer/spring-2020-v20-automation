866d6adcdd4a64c537febe85ea3334fafb3120e2
gurhank <gurhankocyigit@gmail.com>
2020-09-06 11:48:21 -0500

    day3 p2

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P2_ZeroBankVerification.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P2_ZeroBankVerification.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day3_cssSelector_xpath;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class P2_ZeroBankVerification {
@@ -11,10 +13,17 @@ public class P2_ZeroBankVerification {
 //        1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+
 //        2. Go to http://zero.webappsecurity.com/login.html
         driver.get("http://zero.webappsecurity.com/login.html");
+
 //        3. Verify link text from top left:
 //        Expected: “Zero Bank”
+        WebElement zeroBankLinkTopLeft = driver.findElement(By.className("brand"));
+
+        String expectedLinkText = "Zero Bank";
+        String actualLinkText = zeroBankLinkTopLeft.getText();
+
 //        4. Verify link href attribute value contains:
 //        Expected: “index.html”
 
