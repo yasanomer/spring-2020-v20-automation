fb379090522b451afba08ad2b09d5e55b02dc22f
gurhank <gurhankocyigit@gmail.com>
2020-09-06 14:30:25 -0500

    day 3 css selector practice1

---
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelector.java
@@ -0,0 +1,13 @@
+package com.cybertek.tests.day3_cssSelector_xpath;
+
+public class P3_Amazon_cssSelector {
+
+    public static void main(String[] args) {
+        //TC #3: Amazon link number verification
+        //1. Open Chrome browser
+        //2. Go to https://www.amazon.com
+        //3. Enter search term (use cssSelector to locate search box)
+        //4. Verify title contains search term
+    }
+
+}
