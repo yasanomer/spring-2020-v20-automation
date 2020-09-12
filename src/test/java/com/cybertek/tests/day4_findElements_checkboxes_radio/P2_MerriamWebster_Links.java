10b559bc61bf42906f8427ff5db4a1b2f809eba0
gurhank <gurhankocyigit@gmail.com>
2020-09-12 14:35:20 -0500

    practice of findElementS

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P2_MerriamWebster_Links.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P2_MerriamWebster_Links.java
@@ -1,7 +1,11 @@
 package com.cybertek.tests.day4_findElements_checkboxes_radio;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class P2_MerriamWebster_Links {
     public static void main(String[] args) {
@@ -10,7 +14,11 @@ public class P2_MerriamWebster_Links {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         //2. Go to https://www.merriam-webster.com/
         driver.get("https://www.merriam-webster.com/");
-        // 3. Print out the texts of all links
+        //3. Print out the texts of all links
+        //Locating all of the web elements on the page and storing in a list of WebElement
+        List<WebElement> listOfLinks = driver.findElements(By.xpath("//body//a"));
+
+
         //4. Print out how many link is missing text
         //5. Print out how many link has text
         //6. Print out how many total link
