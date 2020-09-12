1ebaa554444f909d6fbd0414aa05fa22fb298268
gurhank <gurhankocyigit@gmail.com>
2020-09-12 15:42:53 -0500

    checkbox practice

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P3_Checkbox_Practice.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes_radio/P3_Checkbox_Practice.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 
 public class P3_Checkbox_Practice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //Practice: Cybertek Checkboxes
         //1. Go to http://practice.cybertekschool.com/checkboxes
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -34,8 +34,25 @@ public class P3_Checkbox_Practice {
         }
 
         //4. Click checkbox #1 to select it.
+        Thread.sleep(1000);
+        checkbox1.click();
+
         //5. Click checkbox #2 to deselect it.
+        Thread.sleep(1000);
+        checkbox2.click();
+
         //6. Confirm checkbox #1 is SELECTED.
+        if (checkbox1.isSelected()){
+            System.out.println("Checkbox 1 is selected. Verification PASSED!");
+        }else{
+            System.out.println("Checkbox 1 NOT is selected. Verification FAILED!!!");
+        }
+
         //7. Confirm checkbox #2 is NOT selected.
+        if (!checkbox2.isSelected()){
+            System.out.println("Checkbox 2 NOT is selected. Verification PASSED!");
+        }else{
+            System.out.println("Checkbox 2 is selected. Verification FAILED!!!");
+        }
     }
 }
