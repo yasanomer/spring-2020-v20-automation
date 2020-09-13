398c4d5b5a06467e6e535e590d4e6b17376e54d2
gurhank <gurhankocyigit@gmail.com>
2020-09-13 14:09:11 -0500

    TestNG Practices class

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -1,10 +1,16 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Practices {
 
-
+    @BeforeMethod
+    public void setUpMethod(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    }
 
     @Test
     public void google_title_verification(){
