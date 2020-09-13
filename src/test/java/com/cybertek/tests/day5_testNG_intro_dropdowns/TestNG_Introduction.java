d3139eafff1040c8ea4a1cf392da5286236920aa
gurhank <gurhankocyigit@gmail.com>
2020-09-13 11:31:32 -0500

    testNG intro

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,4 +1,12 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.Test;
+
 public class TestNG_Introduction {
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running...");
+    }
+
 }
