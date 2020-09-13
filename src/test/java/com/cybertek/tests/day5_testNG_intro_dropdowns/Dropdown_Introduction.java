6f751d2dc0fe50e91efff5092e69943e1183403b
gurhank <gurhankocyigit@gmail.com>
2020-09-13 15:47:24 -0500

    dropdowns

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/Dropdown_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/Dropdown_Introduction.java
@@ -1,4 +1,32 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class Dropdown_Introduction {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        //1- Go to google
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+
+    @Test
+    public void dropdown_test_1(){
+        //TC#1: Verifying “Simple dropdown” and “State selection” dropdown default values
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        //3. Verify “Simple dropdown” default selected value is correct
+        //Expected: “Please select an option”
+        //4. Verify “State selection” default selected value is correct
+        //Expected: “Select a State”
+    }
 }
