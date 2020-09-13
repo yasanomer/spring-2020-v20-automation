f2b6aeef0ae4b35182ae2a7f7a85ea0ac3346359
gurhank <gurhankocyigit@gmail.com>
2020-09-13 14:07:10 -0500

    TestNG Practices class

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Practices {
+
+
+
+    @Test
+    public void google_title_verification(){
+        //1- Go to google
+        //2- Verify title is google
+    }
+
+
+}
