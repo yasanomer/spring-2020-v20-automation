620dfff960e440aa2431f6319062c06ba333f417
gurhank <gurhankocyigit@gmail.com>
2020-09-13 13:48:31 -0500

    testNG intro

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -45,6 +45,10 @@ public class TestNG_Introduction {
 
         Assert.assertTrue(str1.equals(str3) , "Str1 is not equal to Str3.");
 
+        System.out.println("JUst checking if this line will run????? ");
+
+        Assert.assertEquals(str1, str2, "Failure message just in case str1 is not equals to str2");
+
 
     }
 
