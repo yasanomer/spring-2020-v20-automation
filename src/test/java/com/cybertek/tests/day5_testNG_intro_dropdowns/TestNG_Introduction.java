ad43a111bea8347dd9050bddfa84a4a23a8d64ea
gurhank <gurhankocyigit@gmail.com>
2020-09-13 11:47:43 -0500

    testNG intro

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,12 +1,25 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Introduction {
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("Before method is running...");
+    }
 
     @Test
     public void test1(){
         System.out.println("Test1 is running...");
     }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 is running...");
+    }
+
+
 
 }
