c2817f379c6b1281690910a3b48b3967bfaec10d
gurhank <gurhankocyigit@gmail.com>
2020-09-13 12:00:47 -0500

    testNG intro

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -8,6 +9,11 @@ public class TestNG_Introduction {
     @BeforeMethod
     public void setUpMethod(){
         System.out.println("Before method is running...");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("After method is running...");
     }
 
     @Test
