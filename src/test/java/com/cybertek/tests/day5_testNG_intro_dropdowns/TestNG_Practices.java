6f8aa119ff8d470192d7b8af292a5d09d397d29d
gurhank <gurhankocyigit@gmail.com>
2020-09-13 14:26:44 -0500

    TestNG Practices class

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -2,6 +2,8 @@ package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,6 +17,7 @@ public class TestNG_Practices {
     public void setUpMethod(){
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
+
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
     }
@@ -24,8 +27,18 @@ public class TestNG_Practices {
 
         //1- Go to google
         driver.get("https://www.google.com");
-        
+
         //2- Verify title is google
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Titles are not matching!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws InterruptedException{
+        Thread.sleep(3000);
+        driver.close();
     }
 
 
