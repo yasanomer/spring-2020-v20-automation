d028ff383214f15232b16ae020a62d4a88d66941
gurhank <gurhankocyigit@gmail.com>
2020-09-20 10:27:38 -0500

    day 6 setup

---
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/DropdownTasks.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests.day6_dropdown_review_javafaker;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownTasks {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //open a new browser
+        driver = WebDriverFactory.getDriver("chrome");
+        //maximize the page
+        driver.manage().window().maximize();
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //get the page
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+
+
+
+}
