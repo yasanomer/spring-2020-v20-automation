5aef08773acdb8ca7ec0576b036b99058a4d08cb
gurhank <gurhankocyigit@gmail.com>
2020-09-20 15:15:53 -0500

    smart bear utilties

--- src/test/java/com/cybertek/utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/utilities/SmartBearUtilities.java
@@ -1,5 +1,8 @@
 package com.cybertek.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class SmartBearUtilities {
 
     //Mini-Task:
@@ -7,9 +10,18 @@ public class SmartBearUtilities {
     //#2- Create a static method called loginToSmartBear
     //#3- This method simply logs in to SmartBear when you call it.
     //#4- Accepts WebDriver type as parameter
-
     public static void loginToSmartBear(){
-        
+        //3. Enter username: “Tester”
+        WebElement usernameInput = driver.findElement(By.id("ctl00_MainContent_username"));
+        usernameInput.sendKeys("Tester");
+
+        //4. Enter password: “test”
+        WebElement passwordInput = driver.findElement(By.id("ctl00_MainContent_password"));
+        passwordInput.sendKeys("test");
+
+        //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
     }
 
 }
