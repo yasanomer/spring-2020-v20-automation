348286b779cae687a71c093af90be318e5393d3c
gurhank <gurhankocyigit@gmail.com>
2020-09-20 15:14:16 -0500

    smart bear utilties

--- src/test/java/com/cybertek/utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/utilities/SmartBearUtilities.java
@@ -1,4 +1,15 @@
 package com.cybertek.utilities;
 
 public class SmartBearUtilities {
+
+    //Mini-Task:
+    //#1- Create a new class called : SmartBearUtilities
+    //#2- Create a static method called loginToSmartBear
+    //#3- This method simply logs in to SmartBear when you call it.
+    //#4- Accepts WebDriver type as parameter
+
+    public static void loginToSmartBear(){
+        
+    }
+
 }
