b35b975ba606e8a76068fb3406ffccd82b51fffe
gurhank <gurhankocyigit@gmail.com>
2020-09-20 14:47:21 -0500

    Smartbear tasks

--- src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
@@ -53,4 +53,12 @@ public class SmartBearTests {
 
     }
 
+    //Mini-Task:
+    //#1- Create a new class called : SmartBearUtilities
+    //#2- Create a static method called loginToSmartBear
+    //#3- This method simply logs in to SmartBear when you call it.
+    //#4- Accepts WebDriver type as parameter
+    //BREAK UNTIL: 3.10PM CST - 4.10PM EST
+
+
 }
