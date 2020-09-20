f66885137b5aaea9a036dc0695072234295e4d07
gurhank <gurhankocyigit@gmail.com>
2020-09-20 10:34:28 -0500

    day 6 setup

--- src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/DropdownTasks.java
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/DropdownTasks.java
@@ -1,8 +1,11 @@
 package com.cybertek.tests.day6_dropdown_review_javafaker;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,6 +25,17 @@ public class DropdownTasks {
         driver.get("http://practice.cybertekschool.com/dropdown");
     }
 
-
+    //TC #2: Selecting state from State dropdown and verifying result
+    @Test
+    public void test2_verify_state_dropdown(){
+        // we need to locate the dropdown
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        //3. Select Illinois
+        
+        //4. Select Virginia
+        //5. Select California
+        //6. Verify final selected option is California.
+        //Use all Select options. (visible text, value, index)
+    }
 
 }
