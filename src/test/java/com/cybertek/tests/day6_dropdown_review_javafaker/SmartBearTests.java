9bc93e0f75b9752e0840ab3e22d8dfb280b1599c
gurhank <gurhankocyigit@gmail.com>
2020-09-20 14:32:05 -0500

    Smartbear tasks

--- src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day6_dropdown_review_javafaker;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,6 +27,20 @@ public class SmartBearTests {
 
     @Test
     public void task1_login_page_links_print_test(){
+        //3. Enter username: “Tester”
+        WebElement usernameInput = driver.findElement(By.id("ctl00_MainContent_username"));
+        usernameInput.sendKeys("Tester");
+
+        //4. Enter password: “test”
+        WebElement passwordInput = driver.findElement(By.id("ctl00_MainContent_password"));
+        passwordInput.sendKeys("test");
+
+        //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+
+        //6. Print out count of all the links on landing page
+        //7. Print out each link text on this page
 
     }
 
