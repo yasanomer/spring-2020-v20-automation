ef11cb2e409640e9c792e684b45d83180a7c614a
gurhank <gurhankocyigit@gmail.com>
2020-09-22 20:55:34 -0500

    day 7 web tables

--- src/test/java/com/cybertek/tests/day7_webtables/WebTableTasks.java
+++ src/test/java/com/cybertek/tests/day7_webtables/WebTableTasks.java
@@ -2,7 +2,10 @@ package com.cybertek.tests.day7_webtables;
 
 import com.cybertek.utilities.SmartBearUtilities;
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -30,6 +33,20 @@ public class WebTableTasks {
     @Test
     public void p3_verify_susans_order_date(){
 
+        WebElement susansDate = driver.findElement(By.xpath("//td[.='Susan McLaren']/following-sibling::td[3]"));
+
+        //System.out.println("susansDate = " + susansDate.getText());
+
+        String expectedDate = "01/05/2010";
+        String actualDate = susansDate.getText();
+
+        //Assert
+        Assert.assertTrue(actualDate.equals(expectedDate), "Actual date is not equal to expected date");
+
+        Assert.assertEquals(actualDate, expectedDate, "Actual date is not equal to expected date");
+
+       // Assert.fail("I AM FAILING THIS TEST HERE ON PURPOSE");
+        //TODO:FIX THE LOCATOR ASAP
     }
 
 }
