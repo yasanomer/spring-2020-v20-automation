92250ca43ac5297390655b40d32dea7b531db243
gurhank <gurhankocyigit@gmail.com>
2020-09-22 20:35:59 -0500

    day 7 web tables

---
+++ src/test/java/com/cybertek/tests/day7_webtables/WebTableTasks.java
@@ -0,0 +1,19 @@
+package com.cybertek.tests.day7_webtables;
+
+import org.testng.annotations.Test;
+
+public class WebTableTasks {
+
+
+    /*
+    TC#3: Smartbear software order verification
+    1. Open browser and login to Smartbear software
+    2. Click on View all orders
+    3. Verify Susan McLaren has order on date “01/05/2010”
+     */
+    @Test
+    public void p3_verify_susans_order_date(){
+
+    }
+
+}
