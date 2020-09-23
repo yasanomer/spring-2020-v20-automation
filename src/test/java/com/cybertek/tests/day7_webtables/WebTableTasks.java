5c71e39179c58dba6b90724e4d5e617194c55f68
gurhank <gurhankocyigit@gmail.com>
2020-09-22 20:39:53 -0500

    day 7 web tables

--- src/test/java/com/cybertek/tests/day7_webtables/WebTableTasks.java
+++ src/test/java/com/cybertek/tests/day7_webtables/WebTableTasks.java
@@ -1,9 +1,25 @@
 package com.cybertek.tests.day7_webtables;
 
+import com.cybertek.utilities.SmartBearUtilities;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class WebTableTasks {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        SmartBearUtilities.loginToSmartBear(driver);
+    }
 
     /*
     TC#3: Smartbear software order verification
