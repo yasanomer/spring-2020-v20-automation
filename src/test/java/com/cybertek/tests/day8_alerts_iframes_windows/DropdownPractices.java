484aaa8a1bd3bd5894175dc599aa96e0e0c9b6d3
gurhank <gurhankocyigit@gmail.com>
2020-09-27 10:38:59 -0500

    practice 7

---
+++ src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,32 @@
+package com.cybertek.tests.day8_alerts_iframes_windows;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+    }
+
+
+    @Test
+    public void p7_month_dropdown_default_value_test(){
+        //Locating the month dropdown
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+
+    }
+}
