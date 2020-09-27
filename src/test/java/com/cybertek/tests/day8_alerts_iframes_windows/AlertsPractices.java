be62d45e3a4216b9b61de93449ed0c7552038156
gurhank <gurhankocyigit@gmail.com>
2020-09-27 13:47:17 -0500

    alerts practices

--- src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertsPractices.java
+++ src/test/java/com/cybertek/tests/day8_alerts_iframes_windows/AlertsPractices.java
@@ -1,10 +1,12 @@
 package com.cybertek.tests.day8_alerts_iframes_windows;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -35,9 +37,16 @@ public class AlertsPractices {
         //1- Create Alert instance and switch to alert
         Alert alert = driver.switchTo().alert();
 
+        BrowserUtils.wait(2);
+
         //2- Use "alert" instance to accept the javascript alert(popup)
         alert.accept();
 
+        //Locating the result text web element
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+
+        //assert "resultText" is displayed
+        Assert.assertTrue(resultText.isDisplayed(), "Result text is not displayed. Verification failed!!!");
 
     }
 
