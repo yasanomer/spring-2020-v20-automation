3230c20570b5299919851b82adeaf1654ce194b8
gurhank <gurhankocyigit@gmail.com>
2020-10-04 14:50:21 -0500

    using configuration reader

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day10_testbase_properties_driverUtil;
 
+import com.cybertek.utilities.ConfigurationReader;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
@@ -34,6 +35,12 @@ public class ReadingProperties {
 
     }
 
-
+    @Test
+    public void using_properties_utility_method(){
+        //This will read the given "key's" value and return it
+        // as a String
+        System.out.println("ConfigurationReader.getProperty(\"browser\") = " + ConfigurationReader.getProperty("browser"));
+        System.out.println("ConfigurationReader.getProperty(\"smartbearUrl\") = " + ConfigurationReader.getProperty("smartbearUrl"));
+    }
 
 }
