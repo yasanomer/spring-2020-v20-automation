5d17eb806a64a2b40caec0f881a137293925c7b6
gurhank <gurhankocyigit@gmail.com>
2020-10-04 13:14:39 -0500

    learning properties

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/LearningProperties.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/LearningProperties.java
@@ -9,5 +9,12 @@ public class LearningProperties {
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
         System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
 
+        //key=value
+        //os.name=Mac OS X
+        //user.name=cybertekchicago-1
+        // keys has to be exactly the same value when you pass it.
+        // otherwise it will not work. so if you are getting 'null' make sure your key is correct
+
+
     }
 }
---
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
@@ -0,0 +1,4 @@
+package com.cybertek.tests.day10_testbase_properties_driverUtil;
+
+public class ReadingProperties {
+}
