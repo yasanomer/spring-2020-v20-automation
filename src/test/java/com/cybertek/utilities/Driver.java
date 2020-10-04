a92b2892416a5da977111b6d30c0ba43a57f9bd6
gurhank <gurhankocyigit@gmail.com>
2020-10-04 15:32:02 -0500

    Driver util class

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,14 +1,38 @@
 package com.cybertek.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
     private Driver(){}
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
-    
+    public static WebDriver getDriver(){
+
+        if(driver == null){
+
+            String browser = ConfigurationReader.getProperty("browser");
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+            }
+        }
+
+        return driver;
+
+    }
+
+
 
 
 
