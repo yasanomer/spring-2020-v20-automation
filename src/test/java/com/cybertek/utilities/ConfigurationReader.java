38f6e0926f013a6d4fe474bdb701ca77d51d4461
gurhank <gurhankocyigit@gmail.com>
2020-10-04 14:40:22 -0500

    configuration reader class

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -1,5 +1,7 @@
 package com.cybertek.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
@@ -7,9 +9,16 @@ public class ConfigurationReader {
     //#1- We created the properties object
     private static Properties properties = new Properties();
 
-    
-    //#2- We get the path and pass it into FileInputStream, to open the file
-    //#3- We load the opened file into properties object
+
+    static {
+        try {
+            //#2- We get the path and pass it into FileInputStream, to open the file
+            FileInputStream file = new FileInputStream("configuration.properties");
+        } catch (IOException e) {
+            System.out.println("Properties file not found.");
+        }
+        //#3- We load the opened file into properties object
+    }
     //#4- We read from file
     //#5- close the file
 
