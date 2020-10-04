de53ddfb9228dfff67a96e2d621e8cf5f137bde3
gurhank <gurhankocyigit@gmail.com>
2020-10-04 14:30:59 -0500

    configuration reader class

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -1,4 +1,12 @@
 package com.cybertek.utilities;
 
 public class ConfigurationReader {
+
+    //#1- We created the properties object
+    //#2- We get the path and pass it into FileInputStream, to open the file
+    //#3- We load the opened file into properties object
+    //#4- We read from file
+    //#5- close the file
+
+
 }
