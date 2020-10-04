478bda4259e932522f914740090ceecc432df994
gurhank <gurhankocyigit@gmail.com>
2020-10-04 13:15:05 -0500

    configuration.properties

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
@@ -1,4 +1,20 @@
 package com.cybertek.tests.day10_testbase_properties_driverUtil;
 
+import org.testng.annotations.Test;
+
 public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file(){
+
+        // Properties: Use Properties class object
+        // FileInputStream: opens the file
+        // We will load the file
+        // We read from the file
+        // Close the file
+
+    }
+
+
+
 }
