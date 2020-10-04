fd28b0659ffde2a886c1fd788fe53898cadb554a
gurhank <gurhankocyigit@gmail.com>
2020-10-04 10:53:33 -0500

    testbase

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices_9_10_11.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices_9_10_11.java
@@ -1,32 +1,17 @@
 package com.cybertek.tests.day10_testbase_properties_driverUtil;
 
+import com.cybertek.tests.base.TestBase;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.SmartBearUtilities;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-public class SmartBearPractices_9_10_11 {
+public class SmartBearPractices_9_10_11 extends TestBase {
 
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setupMethod(){
-
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        SmartBearUtilities.loginToSmartBear(driver);
-
-    }
     /*
     TC #9: SmartBear delete order
     1. Open browser and login to SmartBear
@@ -35,6 +20,8 @@ public class SmartBearPractices_9_10_11 {
      */
     @Test
     public void p9_delete_order_task(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        SmartBearUtilities.loginToSmartBear(driver);
         //Locate the checkbox for 'Mark Smith' and click to it
         BrowserUtils.wait(2);
         //WebElement markSmithCheckbox = driver.findElement(By.xpath("//td[.='Mark Smith']/preceding-sibling::td[1]"));
