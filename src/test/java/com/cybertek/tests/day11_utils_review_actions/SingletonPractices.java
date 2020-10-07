1071b79e7e800e58f941d56b79746527b3a14cd8
gurhank <gurhankocyigit@gmail.com>
2020-10-06 19:52:00 -0500

    SingletonPractices class

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/SingletonPractices.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/SingletonPractices.java
@@ -8,14 +8,21 @@ public class SingletonPractices {
     public void singleton_test1(){
 
         String str1 = Singleton.getWord();
-        System.out.println("str1 = " + str1);
+        System.out.println("str1 = " + str1); //
 
         String str2 = Singleton.getWord();
         System.out.println("str2 = " + str2);
 
         String str3 = Singleton.getWord();
         System.out.println("str3 = " + str3);
-        
+
+    }
+
+    @Test
+    public void singleton_test2(){
+
+        String str4 = Singleton.getWord();
+        System.out.println("str4 = " + str4);
 
     }
 
