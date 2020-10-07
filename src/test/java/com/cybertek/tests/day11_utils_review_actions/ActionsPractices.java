9784fa583948f353dff7b636cfb64f3222707884
gurhank <gurhankocyigit@gmail.com>
2020-10-06 20:34:43 -0500

    actions practices

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/ActionsPractices.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day11_utils_review_actions;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -18,6 +21,11 @@ public class ActionsPractices {
      */
     @Test
     public void hover_over_test(){
+        //driver
+        //1. Go to http://practice.cybertekschool.com/hovers
+        Driver.getDriver().get("http://practice.cybertekschool.com/hovers");
+
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//div[@class='figure'])[1]"));
 
     }
 
