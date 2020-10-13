2003b26095e6874f2ee2997862c51534549ea80c
gurhank <gurhankocyigit@gmail.com>
2020-10-13 17:19:06 -0500

    actions practices

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day12_review_jsexecutor_pom;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -46,12 +47,16 @@ public class ActionsPractices {
     public void p3_drag_and_drop(){
         //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
         Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        Driver.getDriver().manage().window().maximize();
 
         //2. Drag and drop the small circle to bigger circle.
-
+        BrowserUtils.wait(3);
+        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
+        acceptCookiesButton.click();
         //Locating circles
         WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
         WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
+        BrowserUtils.wait(3);
 
         Actions actions = new Actions(Driver.getDriver());
         //DragAndDrop method expects to arguments:
@@ -61,6 +66,15 @@ public class ActionsPractices {
 
         //3. Assert:
         //-Text in big circle changed to: “You did great
+        String expected = "You did great!";
+        String actual = bigCircle.getText();
+
+        Assert.assertTrue(actual.equals(expected));
+
+
+
+        BrowserUtils.wait(10);
+        Driver.closeDriver();
     }
 
 
