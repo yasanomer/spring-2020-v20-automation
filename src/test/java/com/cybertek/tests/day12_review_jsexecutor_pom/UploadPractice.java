a463868ff5d747bb1c0e32da8185c92a9e657c58
gurhank <gurhankocyigit@gmail.com>
2020-10-13 18:33:25 -0500

    upload practice

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/UploadPractice.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/UploadPractice.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day12_review_jsexecutor_pom;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -20,11 +21,21 @@ public class UploadPractice {
         //Windows: Right click -> properties -> copy from location
         //OR SHIFT + RIGHT CLICK --> Copy as path
         String path = "/Users/cybertekchicago-1/Desktop/warface.jpg";
+        ///Users/cybertekchicago-1/Desktop/warface.jpg
+        //String windowsPath = "C:\\Users\\direc\\Desktop\\Sample.xlsx";
 
         //Locate the upload web element
         WebElement uploadInput = Driver.getDriver().findElement(By.id("file-upload"));
 
         //send the path of the file to uploadInput webElement
+        BrowserUtils.wait(3);
         uploadInput.sendKeys(path);
+
+        //locate the UPLOAD button
+        WebElement uploadButton = Driver.getDriver().findElement(By.id("file-submit"));
+
+        //click to upload button
+        BrowserUtils.wait(2);
+        uploadButton.click();
     }
 }
