32a54d4a531c42f3085a51c716543f28a1bc89bd
gurhank <gurhankocyigit@gmail.com>
2020-10-13 16:28:08 -0500

    actions practices

---
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day12_review_jsexecutor_pom;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    /*
+    TC #16: Double Click Test
+    1. Go to https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
+    2. Switch to iframe.
+    3. Double click on the text “Double-click me to change my text color.”
+    4. Assert: Text’s “style” attribute value contains “red”.
+     */
+    @Test
+    public void p2_double_click_test(){
+
+
+
+
+    }
+
+
+}
