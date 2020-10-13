151f5be9d2101bde2d03c2b2e3f5493b4cc7b8de
gurhank <gurhankocyigit@gmail.com>
2020-10-13 17:29:26 -0500

    actions practices

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
@@ -77,5 +77,51 @@ public class ActionsPractices {
         Driver.closeDriver();
     }
 
+    @Test
+    public void p3_drag_and_drop_LONGERWAY(){
+        //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        Driver.getDriver().manage().window().maximize();
+
+        //2. Drag and drop the small circle to bigger circle.
+        BrowserUtils.wait(3);
+        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
+        acceptCookiesButton.click();
+        //Locating circles
+        WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
+        WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
+        BrowserUtils.wait(3);
+
+        Actions actions = new Actions(Driver.getDriver());
+        //DragAndDrop method expects to arguments:
+        // #1-Which webElement you want to drag : SOURCE
+        // #2-Where do you want to drop it? : TARGET
+        //actions.dragAndDrop(smallCircle, bigCircle).perform();
+
+        //actions.clickAndHold(smallCircle).moveToElement(bigCircle).release().perform();
+        actions.clickAndHold(smallCircle).perform();
+        BrowserUtils.wait(3);
+
+        actions.moveToElement(bigCircle).perform();
+        BrowserUtils.wait(3);
+
+        actions.release().perform();
+
+
+
+
+        //3. Assert:
+        //-Text in big circle changed to: “You did great
+        String expected = "You did great!";
+        String actual = bigCircle.getText();
+
+        Assert.assertTrue(actual.equals(expected));
+
+
+
+        BrowserUtils.wait(10);
+        Driver.closeDriver();
+    }
+
 
 }
