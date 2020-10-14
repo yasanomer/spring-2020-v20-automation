9fc352d83b9b3a810df933bab679b3f2047bec40
gurhank <gurhankocyigit@gmail.com>
2020-10-13 19:06:29 -0500

    js executor practices

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/JavaScriptExecutor_Practices.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/JavaScriptExecutor_Practices.java
@@ -1,12 +1,18 @@
 package com.cybertek.tests.day12_review_jsexecutor_pom;
 
+import com.cybertek.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class JavaScriptExecutor_Practices {
 
     @Test
     public void scroll_using_jsexecutor_1(){
+        //Get the page to scroll
+        Driver.getDriver().get("http://practice.cybertekschool.com/infinite_scroll");
+
+        //Create instance of JSExecutor and cast our driver type to it.
         
+
     }
 
 }
