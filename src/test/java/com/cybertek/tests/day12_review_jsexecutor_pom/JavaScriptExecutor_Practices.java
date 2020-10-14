2ab3b1e7a1d938bd98cbbbde418bd759b3effd5d
gurhank <gurhankocyigit@gmail.com>
2020-10-13 19:04:19 -0500

    js executor practices

---
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/JavaScriptExecutor_Practices.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day12_review_jsexecutor_pom;
+
+import org.testng.annotations.Test;
+
+public class JavaScriptExecutor_Practices {
+
+    @Test
+    public void scroll_using_jsexecutor_1(){
+        
+    }
+
+}
