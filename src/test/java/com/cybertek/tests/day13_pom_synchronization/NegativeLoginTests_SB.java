dc33b23828a8b264a8cf4c36af88ff7f88d82bf4
gurhank <gurhankocyigit@gmail.com>
2020-10-14 19:22:06 -0500

    changes

--- src/test/java/com/cybertek/tests/day13_pom_synchronization/NegativeLoginTests_SB.java
+++ src/test/java/com/cybertek/tests/day13_pom_synchronization/NegativeLoginTests_SB.java
@@ -1,8 +1,18 @@
 package com.cybertek.tests.day13_pom_synchronization;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class NegativeLoginTests_SB {
 
+    @Test (description = "entering incorrect username")
+    public void negative_login_test1(){
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("smartbearUrl"));
+
+
+    }
 
 
 
